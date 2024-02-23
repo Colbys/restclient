0e7967520885f76d7377970ed542b1c7b373b3eb
Alexander Ionov <alexander.ionov97@gmail.com>
2024-02-23 13:24:36 +0300

    Initial Commit

---
+++ restclient/restclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  restclientApp.swift
+//  restclient
+//
+//  Created by Alexander Ionov on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct restclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
